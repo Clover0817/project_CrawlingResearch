cd57a332bba3d611ae1f10d0e32a20599e7e5d51
Nayeon Kim <skdus3373@gmail.com>
2023-05-18 14:40:44 +0900

    update: 쿠팡, 지마켓 os_mode 추가 및 info 파일 변경

---
+++ 20230518_Update/CrawlEcommerce/loginInfo.py
@@ -0,0 +1,11 @@
+def login(site_selection):
+    if site_selection == 0:
+            id = ""
+            password = ""
+    elif site_selection == 1:
+            id = ""
+            password = ""
+    else:
+        print("로그인 오류")
+
+    return id, password
